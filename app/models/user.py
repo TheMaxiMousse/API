c30d6d6ad7d93050de7d727f898ebe7c0712f0b3
Vianpyro <vianney@veremme.org>
2025-06-08 22:18:55 +0000

    Add __table_args__ to User model to extend existing table definition

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,7 @@ from app.models import Base
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
 
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
